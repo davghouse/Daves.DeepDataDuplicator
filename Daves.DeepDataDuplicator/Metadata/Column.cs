224fc1481a71a9db882c9db3528300bf3f34e6e2
galehouse5 <galehouse.5@gmail.com>
2017-07-06 10:17:32 -0400

    Added handling for timestamp columns.
    A timestamp column doesn't allow a value to be explicitly inserted
    because it tracks when a row was last updated. We can't copy timestamp
    values so just ignore them.

--- Daves.DeepDataDuplicator/Metadata/Column.cs
+++ Daves.DeepDataDuplicator/Metadata/Column.cs
@@ -6,11 +6,11 @@ namespace Daves.DeepDataDuplicator.Metadata
 {
     public class Column
     {
-        public Column(object tableId, object name, object columnId, object isNullable, object isIdentity, object isComputed)
-            : this((int)tableId, (string)name, (int)columnId, (bool)isNullable, (bool)isIdentity, (bool)isComputed)
+        public Column(object tableId, object name, object columnId, object isNullable, object isIdentity, object isComputed, object isTimestamp)
+            : this((int)tableId, (string)name, (int)columnId, (bool)isNullable, (bool)isIdentity, (bool)isComputed, (bool)isTimestamp)
         { }
 
-        public Column(int tableId, string name, int columnId, bool isNullable, bool isIdentity = false, bool isComputed = false)
+        public Column(int tableId, string name, int columnId, bool isNullable, bool isIdentity = false, bool isComputed = false, bool isTimestamp = false)
         {
             TableId = tableId;
             Name = name;
@@ -18,6 +18,7 @@ namespace Daves.DeepDataDuplicator.Metadata
             IsNullable = isNullable;
             IsIdentity = isIdentity;
             IsComputed = isComputed;
+            IsTimestamp = isTimestamp;
         }
 
         public int TableId { get; }
@@ -26,6 +27,7 @@ namespace Daves.DeepDataDuplicator.Metadata
         public bool IsNullable { get; }
         public bool IsIdentity { get; }
         public bool IsComputed { get; }
+        public bool IsTimestamp { get; }
         public Table Table { get; protected set; }
 
         public virtual void Initialize(IReadOnlyList<Table> tables)
@@ -35,7 +37,7 @@ namespace Daves.DeepDataDuplicator.Metadata
             => Name.ToLowercaseSpacelessName();
 
         public virtual bool IsCopyable
-            => !IsIdentity && !IsComputed;
+            => !IsIdentity && !IsComputed && !IsTimestamp;
 
         public override string ToString()
             => $"{Table}: {Name}";
